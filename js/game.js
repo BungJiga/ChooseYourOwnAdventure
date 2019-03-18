4be311890b00fc566eba7f467b2072d07aaf0f08
Michael Guerrero <michaelguerrero@uagateway.org>
2019-03-18 21:31:27 +0000

    Scooby Doo!

--- js/game.js
+++ js/game.js
@@ -1,29 +1,45 @@
 // NOTE - You must have a level with the name "start".  This is used as the first level in the game.
 
 var game = {
-    music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    music: "hedwigs-theme.mp3",
+    background_image: "house.jpg",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You're Harry Potter and you have been invited to Hogwarts to learn magic. Do you accept?",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Stay Home",
+                    nextLevel: "dissapointment",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Go to Hogwarts",
+                    nextLevel: "train",
                 },
             ]
         },
 
-        cave: {
-            background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+        train: {
+            background_image: "train.jpg",
+            music: "train.mp3",
+            message: "You are on the train to Hogwarts. There is a Dementor coming at you what do you do? ",
+            choices: [
+                {
+                    text: "Use magic",
+                    nextLevel: "magic",
+                },
+                 {
+                    text: "Hide somewhere",
+                    nextLevel: "safe",
+                },
+            ]
+        },
+
+        dissapointment: {
+            background_image: "stairs.png",
+            music: "evil.mp3",
+            message: "You live your life as a dissapointment to yourself.",
             choices: [
                 {
                     text: "Start over",
@@ -31,16 +47,60 @@ var game = {
                 },
             ]
         },
-
-        field: {
-            message: "Some adventurer you are...",
+        
+        dissapointment_two: {
+            background_image: "stairs.png",
+            music: "evil.mp3",
+            message: "You gave up the opportunity of a lifetime and Voldermort kills you by flicking you.",
             choices: [
                 {
                     text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+        magic: {
+            background_image: "dad.jpg",
+            music: "evil.mp3",
+            message: "It sucked your soul and you are now expelled for trying to magic outside of school :( .",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+        
+        safe: {
+            background_image: "train.jpg",
+            music: "train.mp3",
+            message: "Your safe and you continue your ride to Hogwarts",
+            choices: [
+                {
+                    text: "Continue to Hogwarts",
+                    nextLevel: "hogwarts",
+                },
+                 {
+                    text: "Run away",
+                    nextLevel: "dissapointment_two",
+                },
+            ]
+        },
+        
+        hogwarts: {
+            background_image: "hogwarts.jpg",
+            music: "hedwigs-theme.mp3",
+            message: "You make it to Hogwarts and you continue to study magic and eventually defeat Voldermort and gain all the glory ;)",
+            choices: [
+                {
+                    text: "Start Over",
                     nextLevel: "start",
                 },
             ]
         },
 
     }
+    
+    
 };
